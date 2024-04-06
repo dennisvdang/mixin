18db695b3f9497daab754c61ce86ac1487d2cc1e
Dennis Dang <dennisvdang@gmail.com>
2024-04-06 15:01:18 -0700

    Update directory structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='AI-powered DJ that generates mixtapes from a playlist of songs',
+    author='Dennis Dang',
+    license='MIT',
+)
